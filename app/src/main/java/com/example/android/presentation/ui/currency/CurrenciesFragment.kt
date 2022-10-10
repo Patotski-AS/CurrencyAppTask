78bbaf19a04d1db255dfeb615830e6e4fa150671
Patotski Andrey <7906241@gmail.com>
2022-10-10 20:13:14 +0300

    feat: inject CurrenciesViewModel

--- app/src/main/java/com/example/android/presentation/ui/currency/CurrenciesFragment.kt
+++ app/src/main/java/com/example/android/presentation/ui/currency/CurrenciesFragment.kt
@@ -5,12 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.android.databinding.FragmentCurrenciesBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class CurrenciesFragment : Fragment() {
 
     private var _binding: FragmentCurrenciesBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: CurrenciesViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
